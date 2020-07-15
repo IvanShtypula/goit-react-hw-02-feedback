d89809e2e7f0b236505c55bd9771d520da7fe8ea
Mypaxa4e <ivanshtypula@gmail.com>
2020-07-15 17:16:25 +0300

    Updates

---
+++ precache-manifest.3d3666fb0d74de890b06c68e9860fa57.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b98a3b3f2d95a4eecd63bb2c5c65c682",
+    "url": "/goit-react-hw-02-feedback/index.html"
+  },
+  {
+    "revision": "22179f3c18c97478a424",
+    "url": "/goit-react-hw-02-feedback/static/css/main.70c803a3.chunk.css"
+  },
+  {
+    "revision": "f1086c5a804457390e65",
+    "url": "/goit-react-hw-02-feedback/static/js/2.3a8075e9.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/goit-react-hw-02-feedback/static/js/2.3a8075e9.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "22179f3c18c97478a424",
+    "url": "/goit-react-hw-02-feedback/static/js/main.fc31730c.chunk.js"
+  },
+  {
+    "revision": "25083be5abe132dedb91",
+    "url": "/goit-react-hw-02-feedback/static/js/runtime-main.e7f9aa6f.js"
+  }
+]);
